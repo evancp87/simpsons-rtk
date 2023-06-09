08e364e756e31c79c71e7d2ba5e58e367cf92e34
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-09 19:06:10 +0100

    functional homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,35 @@
-/* eslint-disable react/prop-types */
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Loading from "./components/Loading";
-import Simpsons from "./components/Simpsons";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import Error404 from "./components/Error404";
-import Nav from "./components/Nav";
-import Screen from "./components/Screen";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const [showSplash, setShowSplash] = useState(true);
-  const [data, setData] = useState(null);
-  const [total, setTotal] = useState(0);
-
-  // gets api data and renders splash screen
-  const getData = async () => {
-    try {
-      const { data } = await axios.get(
-        "https://thesimpsonsquoteapi.glitch.me/quotes?count=10"
-      );
-
-      data.forEach((element, index) => {
-        element.id = index + Math.random();
-      });
-      data.forEach((element) => {
-        element.liked = false;
-        console.log(element);
-      });
-
-      setData(data);
-      setTimeout(() => {
-        setShowSplash(false);
-      }, 3000);
-    } catch (error) {
-      console.log("The error is:", error);
-    }
-  };
-  // api call
-  useEffect(() => {
-    getData();
-  }, []);
-
-  // increments likes count if character is liked
-  useEffect(() => {
-    if (data) {
-      let count = 0;
-      data.forEach((char) => {
-        if (char.liked) count++;
-      });
-      setTotal(count);
-    }
-  }, [data]);
-
-  if (!data) return <Loading />;
-
-  if (data.length === 0) return <p>Doh! You ran out of Springfieldians!</p>;
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <Router>
-      {showSplash ? (
-        <Screen />
-      ) : (
-        <>
-          <p>Total no of liked chars: {total} </p>
-          <Nav />
-          <Routes>
-            <Route
-              path="/"
-              element={<Simpsons data={data} setData={setData} />}
-            />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Error404 />} />
-          </Routes>
-        </>
-      )}
-    </Router>
-  );
-};
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
